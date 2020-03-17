809c8ed6f07fdc48c0ddf7569512e527e030ba83
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2020-03-17 10:28:53 +0100

    Add project files.

---
+++ 01_Kalkulator/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _01_Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxValue1 = new System.Windows.Forms.TextBox();
+            this.textBoxValue2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.comboBoxSign = new System.Windows.Forms.ComboBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxValue1
+            // 
+            this.textBoxValue1.Location = new System.Drawing.Point(13, 38);
+            this.textBoxValue1.Name = "textBoxValue1";
+            this.textBoxValue1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue1.TabIndex = 0;
+            this.textBoxValue1.Text = "1";
+            this.textBoxValue1.TextChanged += new System.EventHandler(this.sourceValueChanged);
+            // 
+            // textBoxValue2
+            // 
+            this.textBoxValue2.Location = new System.Drawing.Point(195, 38);
+            this.textBoxValue2.Name = "textBoxValue2";
+            this.textBoxValue2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue2.TabIndex = 2;
+            this.textBoxValue2.Text = "1";
+            this.textBoxValue2.TextChanged += new System.EventHandler(this.sourceValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Liczba 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Liczba 2";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Enabled = false;
+            this.textBoxResult.Location = new System.Drawing.Point(13, 93);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(282, 20);
+            this.textBoxResult.TabIndex = 6;
+            // 
+            // comboBoxSign
+            // 
+            this.comboBoxSign.FormattingEnabled = true;
+            this.comboBoxSign.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBoxSign.Location = new System.Drawing.Point(120, 38);
+            this.comboBoxSign.Name = "comboBoxSign";
+            this.comboBoxSign.Size = new System.Drawing.Size(69, 21);
+            this.comboBoxSign.TabIndex = 1;
+            this.comboBoxSign.Text = "+";
+            this.comboBoxSign.TextChanged += new System.EventHandler(this.sourceValueChanged);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(13, 64);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(282, 23);
+            this.buttonCalculate.TabIndex = 3;
+            this.buttonCalculate.Text = "Licz";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.sourceValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 126);
+            this.Controls.Add(this.comboBoxSign);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxValue2);
+            this.Controls.Add(this.textBoxValue1);
+            this.MaximumSize = new System.Drawing.Size(323, 165);
+            this.MinimumSize = new System.Drawing.Size(323, 165);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxValue1;
+        private System.Windows.Forms.TextBox textBoxValue2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.ComboBox comboBoxSign;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ 01_Kalkulator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //żeby było postarcie programu policzone dla wartości domyślnych (1+1)
+            sourceValueChanged(null, null);
+        }
+
+        private void sourceValueChanged(object sender, EventArgs e)
+        {
+            bool isOK = true;
+
+            textBoxValue1.BackColor = System.Drawing.SystemColors.Window;
+            textBoxValue2.BackColor = System.Drawing.SystemColors.Window;
+            comboBoxSign.BackColor = System.Drawing.SystemColors.Window;
+
+            if (!Int32.TryParse(textBoxValue1.Text, out int x))
+            {
+                textBoxValue1.BackColor = Color.Red;
+                isOK = false;
+            }
+            if (!Int32.TryParse(textBoxValue2.Text, out int y))
+            {
+                textBoxValue2.BackColor = Color.Red;
+                isOK = false;
+            }
+            if (!(new String[] { "+", "-", "*", "/" }).Contains(comboBoxSign.Text))
+            {
+                comboBoxSign.BackColor = Color.Red;
+                isOK = false;
+            }
+
+            if (isOK)
+            {
+                switch (comboBoxSign.Text)
+                {
+                    case "+":
+                        textBoxResult.Text = (x + y).ToString();
+                        break;
+                    case "-":
+                        textBoxResult.Text = (x - y).ToString();
+                        break;
+                    case "*":
+                        textBoxResult.Text = (x * y).ToString();
+                        break;
+                    case "/":
+                        if (y != 0)
+                        {
+                            textBoxResult.Text = (x / y).ToString();
+                        }
+                        else
+                        {
+                            textBoxValue2.BackColor = Color.Red;
+                            textBoxResult.Text = "";
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                textBoxResult.Text = "";
+            }
+
+        }
+    }
+}
